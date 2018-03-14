a54f941fbbd1b368cac0c708011395efe911d252
gx0609m <1058742912@qq.com>
2018-03-14 13:14:36 +0800

    broadcast

--- app/src/main/java/com/example/administrator/androiddemo/broadcast/BroadcastLearningActivity.java
+++ app/src/main/java/com/example/administrator/androiddemo/broadcast/BroadcastLearningActivity.java
@@ -35,10 +35,6 @@ public class BroadcastLearningActivity extends AppCompatActivity implements View
     /**
      * 动态注册广播(不要忘记unRegisterReceiver)
      *
-     * 在实际应用中，我们在Activity或Service中注册了一个BroadcastReceiver，
-     * 当这个Activity或Service被销毁时如果没有解除注册，
-     * 系统会报一个异常，提示我们是否忘记解除注册了，所以，记得在特定的地方执行解除注册操作
-     *
      * 静态注册广播只需将此段代码替换成AndroidManifest.xml中的：（静态注册就是在AndroidManifest.xml配置文件中注册）
      *      <receiver android:name="com.example.administrator.androiddemo.broadcast.MyBroadcastReceiver">
      *           <intent-filter>
@@ -47,7 +43,9 @@ public class BroadcastLearningActivity extends AppCompatActivity implements View
      *      </receiver>
      *
      * 需要注意的是：
-     *      动态注册的广播是跟随当前Context的生命周期的；
+     *      动态注册的广播是跟随当前Context的生命周期的，这种广播具有组件级别的效果，
+     *      即当我们调用unregisterReceiver()方法或组件销毁广播就无法被接收处理了；
+     *
      *      静态注册的广播是常驻型的，即意味着，即使当前应用关闭了，
      *      如果有带有Constant.MY_BROADCAST_RECEIVER_ACTION 这个action的广播发来，MyBroadcastReceiver也会被调用运行（此特性有很多用处）；
      */
@@ -64,12 +62,22 @@ public class BroadcastLearningActivity extends AppCompatActivity implements View
                 // 发送广播 action可以理解为广播地址
                 Intent intent = new Intent();
                 intent.setAction(Constant.MY_BROADCAST_RECEIVER_ACTION);
-                intent.putExtra("data","data"); // 发送广播时可以传送数据
+                // 发送广播时可以传送数据
+                intent.putExtra("data","data");
+                // 发送广播可以通过Context的sendBroadcast()或sendOrderedBroadcast()发送普通广播或有序广播
                 sendBroadcast(intent);
                 break;
         }
     }
 
+    /**
+     * 在适合的生命周期中，解除注册
+     *
+     * 在实际应用中，我们在Activity或Service中注册了一个BroadcastReceiver，
+     * 当这个Activity或Service被销毁时如果没有解除注册，
+     * 系统会报一个异常，提示我们是否忘记解除注册了，所以，记得在特定的地方执行解除注册操作
+     *
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
