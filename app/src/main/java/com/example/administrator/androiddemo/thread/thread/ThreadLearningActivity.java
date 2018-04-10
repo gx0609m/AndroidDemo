0b8597f175fad7578281b989fee72867f28d0528
gx0609m <1058742912@qq.com>
2018-04-10 18:04:57 +0800

    threadpool

--- app/src/main/java/com/example/administrator/androiddemo/thread/thread/ThreadLearningActivity.java
+++ app/src/main/java/com/example/administrator/androiddemo/thread/thread/ThreadLearningActivity.java
@@ -12,10 +12,23 @@ import com.example.administrator.androiddemo.base.BaseActivity;
 
 public class ThreadLearningActivity extends BaseActivity {
 
+    /*
+     * 创建新Thread的两种方式：
+     *      1.继承Thread类；
+     *      2.实现Runnable接口；
+     */
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_learning);
+
+    }
+
+    /**
+     * 继承Thread方式来新建线程
+     */
+    private void withExtendsThread(){
 
     }
 }
