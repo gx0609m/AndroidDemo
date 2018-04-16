5b7df3a4bb369214e3933e96c21510b2404d1c81
gx0609m <1058742912@qq.com>
2018-04-16 22:01:11 +0800

    custom view

---
+++ app/src/main/java/com/example/administrator/androiddemo/customview/CustomViewLearningActivity.java
@@ -0,0 +1,22 @@
+package com.example.administrator.androiddemo.customview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.administrator.androiddemo.R;
+import com.example.administrator.androiddemo.base.BaseActivity;
+
+/**
+ * Created by gx on 2018/4/16 0016
+ */
+
+public class CustomViewLearningActivity extends BaseActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view_learning);
+    }
+}
