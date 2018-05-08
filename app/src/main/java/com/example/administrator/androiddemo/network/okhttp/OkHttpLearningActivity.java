62f7bcbc925d2778cc2a9f178b53d2fad59ac1d5
gx0609m <1058742912@qq.com>
2018-05-08 16:28:19 +0800

    okhttp

--- app/src/main/java/com/example/administrator/androiddemo/network/okhttp/OkHttpLearningActivity.java
+++ app/src/main/java/com/example/administrator/androiddemo/network/okhttp/OkHttpLearningActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.androiddemo.network.okhttp;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
@@ -13,14 +14,19 @@ import com.example.administrator.androiddemo.model.MovieEntity;
 import com.example.administrator.androiddemo.utils.ThreadPoolUtil;
 import com.google.gson.Gson;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.BufferedSink;
 
 /**
  * OkHttp
@@ -32,8 +38,11 @@ public class OkHttpLearningActivity extends BaseActivity implements View.OnClick
 
     private static final String TAG = "OkHttpLearningActivity";
 
-    private Button syncGetReq;
-    private Button asyncGetReq;
+    private Button syncGetReq;   // 同步Get请求
+    private Button asyncGetReq;  // 异步Get请求
+    private Button postAsyncUploadKeyValue;  // Post异步上传键值对
+    private Button postAsyncUploadFile;  // Post异步上传文件
+    private Button postAsyncUploadStream;  // Post异步上传流
 
     private String baseUrl = "https://api.douban.com/v2/movie/";
     private int start = 0;
@@ -138,6 +147,145 @@ public class OkHttpLearningActivity extends BaseActivity implements View.OnClick
         });
     }
 
+    /**
+     * Post异步上传键值对（1、2、3、4、5）
+     */
+    private void asyncUploadKeyValueWithPost() {
+        /*
+         * 1.创建 okHttpClient 实例
+         */
+        OkHttpClient okHttpClient = new OkHttpClient();
+        /*
+         * 2.构建表单 formBody 实例 ——— 相较于Get请求，多了此步骤
+         */
+        FormBody formBody = new FormBody.Builder()
+                .add("name", "gx")
+                .build();
+        /*
+         * 3.构建请求 request 实例
+         */
+        Request request = new Request.Builder().url("http://www.baidu.com")
+                .post(formBody)
+                .build();
+        /*
+         * 4.构建 call 对象实例
+         */
+        Call call = okHttpClient.newCall(request);
+        /*
+         * 5.异步网络请求
+         */
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // 请求失败
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // 请求成功
+            }
+        });
+    }
+
+    /**
+     * Post异步上传文件（1、2、3、4、5）
+     */
+    private void asyncUploadFileWithPost() {
+        /*
+         * 1.创建 okHttpClient 实例
+         */
+        OkHttpClient okHttpClient = new OkHttpClient();
+        /*
+         * 2.创建 RequestBody 以及所需的参数 ——— 相较于Get请求，多了此步骤
+         *    a.获取文件
+         *    b.创建 MediaType 设置上传文件类型
+         *    c.获取请求体
+         */
+        File file = new File(Environment.getExternalStorageDirectory() + "test.txt");
+        MediaType MEDIATYPE = MediaType.parse("text/plain; charset=utf-8");
+        RequestBody requestBody = RequestBody.create(MEDIATYPE, file);
+        /*
+         * 3.构建请求 request 实例
+         */
+        Request request = new Request.Builder().url("http://www.baidu.com")
+                .post(requestBody)
+                .build();
+        /*
+         * 4.构建 call 对象实例
+         */
+        Call call = okHttpClient.newCall(request);
+        /*
+         * 5.异步网络请求
+         */
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // 请求失败
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // 请求成功
+            }
+        });
+    }
+
+    /**
+     * Post异步上传流（1、2、3、4、5）
+     */
+    private void asyncUploadStreamWithPost() {
+        /*
+         * 1.创建 okHttpClient 实例
+         */
+        OkHttpClient okHttpClient = new OkHttpClient();
+        /*
+         * 2.创建 RequestBody 以及所需的参数 ——— 相较于Get请求，多了此步骤
+         */
+        final MediaType MEDIA_TYPE_TEXT = MediaType.parse("text/plain");
+        final String postBody = "Hello World";
+        RequestBody requestBody = new RequestBody() {
+            @Override
+            public MediaType contentType() {
+                return MEDIA_TYPE_TEXT;
+            }
+
+            @Override
+            public void writeTo(BufferedSink sink) throws IOException {
+                sink.writeUtf8(postBody);
+            }
+
+            @Override
+            public long contentLength() throws IOException {
+                return postBody.length();
+            }
+        };
+        /*
+         * 3.构建请求 request 实例
+         */
+        Request request = new Request.Builder()
+                .url("http://www.baidu.com")
+                .post(requestBody)
+                .build();
+         /*
+         * 4.构建 call 对象实例
+         */
+        Call call = okHttpClient.newCall(request);
+        /*
+         * 5.异步网络请求
+         */
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // 请求失败
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // 请求成功
+            }
+        });
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -146,6 +294,15 @@ public class OkHttpLearningActivity extends BaseActivity implements View.OnClick
                 break;
             case R.id.asyncGetReq:
                 asyncGetRequest();
+                break;
+            case R.id.postAsyncUploadKeyValue:
+                asyncUploadKeyValueWithPost();
+                break;
+            case R.id.postAsyncUploadFile:
+                asyncUploadFileWithPost();
+                break;
+            case R.id.postAsyncUploadStream:
+                asyncUploadStreamWithPost();
                 break;
         }
     }
@@ -156,5 +313,14 @@ public class OkHttpLearningActivity extends BaseActivity implements View.OnClick
 
         asyncGetReq = (Button) findViewById(R.id.asyncGetReq);
         asyncGetReq.setOnClickListener(this);
+
+        postAsyncUploadKeyValue = (Button) findViewById(R.id.postAsyncUploadKeyValue);
+        postAsyncUploadKeyValue.setOnClickListener(this);
+
+        postAsyncUploadFile = (Button) findViewById(R.id.postAsyncUploadFile);
+        postAsyncUploadFile.setOnClickListener(this);
+
+        postAsyncUploadStream = (Button) findViewById(R.id.postAsyncUploadStream);
+        postAsyncUploadStream.setOnClickListener(this);
     }
 }
